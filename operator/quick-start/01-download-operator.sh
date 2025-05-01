36f136071819760bef5815ed62ec1dd00affff09
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2025-05-01 13:40:26 +0300

    docs/operator/quick-start

--- operator/quick-start/01-download-operator.sh
+++ operator/quick-start/01-download-operator.sh
@@ -5,6 +5,7 @@ set -x
 
 export VM_OPERATOR_VERSION=$(basename $(curl -fs -o /dev/null -w %{redirect_url} \
   https://github.com/VictoriaMetrics/operator/releases/latest))
+echo "VM_OPERATOR_VERSION=$VM_OPERATOR_VERSION"
 
 wget -O operator-and-crds.yaml \
   "https://github.com/VictoriaMetrics/operator/releases/download/$VM_OPERATOR_VERSION/install-no-webhook.yaml"
