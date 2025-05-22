2823fbe446ff640f459dbce0ceecae144ba3827b
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2025-05-22 20:19:05 +0300

    wip

---
+++ operator/integrations/istio/00-install-vm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+../../quick-start/01-download-operator.sh
+../../quick-start/02-install-operator.sh
+../../quick-start/10-create-vmsingle-manifest.sh
+../../quick-start/11-install-vmsingle.sh
+../../quick-start/20-create-vmagent-manifest.sh
+../../quick-start/21-install-vmagent.sh
+../../quick-start/30-create-demo-app-manifest.sh
+../../quick-start/32-create-demo-app-service-scrape.sh
