ff2e8c7a51c51265196204551e4580ced467c683
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2025-05-30 15:46:26 +0300

    operator istio integration

--- operator/integrations/istio/00-install-istio.sh
+++ operator/integrations/istio/00-install-istio.sh
@@ -3,6 +3,5 @@
 set -e
 set -x
 
-curl -L https://istio.io/downloadIstio | sh -;
-
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.26.0 sh -;
 ./istio-1.26.0/bin/istioctl install -f ./istio-1.26.0/samples/bookinfo/demo-profile-no-gateways.yaml -y;
